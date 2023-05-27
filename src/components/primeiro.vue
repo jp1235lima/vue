ae89e1086c0f81dbe8f26db97eed4f7be24585be
JoaoSpera <joaospera38@gmail.com>
2023-05-26 22:58:06 -0300

    first commit

---
+++ src/components/primeiro.vue
@@ -0,0 +1,20 @@
+<script>
+export default {
+  data() {
+    return {
+      awesome: true
+    }
+  },
+  methods: {
+    toggle() {
+      // ...
+    }
+  }
+}
+</script>
+
+<template>
+  <button @click="toggle">toggle</button>
+  <h1>Olá Mundo!!!</h1>
+  <h1>Tentando fazer algo no Vue</h1>
+</template>
---
+++ src/components/segundo.vue
@@ -0,0 +1,52 @@
+<script>
+let id = 0
+
+export default {
+  data() {
+    return {
+      newTodo: '',
+      hideCompleted: false,
+      todos: [
+        { id: id++, text: 'Estudar Matemática', done: false },
+        { id: id++, text: 'Estudar JavaScript', done: false },
+        { id: id++, text: 'Estudar Banco de Dados', done: false }
+      ]
+    }
+  },
+  computed: {
+    // ...
+  },
+  methods: {
+    addTodo() {
+      this.todos.push({ id: id++, text: this.newTodo, done: false })
+      this.newTodo = ''
+    },
+    removeTodo(todo) {
+      this.todos = this.todos.filter((t) => t !== todo)
+    }
+  }
+}
+</script>
+
+<template>
+  <form @submit.prevent="addTodo">
+    <input v-model="newTodo">
+    <button>Adicionar</button>
+  </form>
+  <ul>
+    <li v-for="todo in todos" :key="todo.id">
+      <input type="checkbox" v-model="todo.done">
+      <span :class="{ done: todo.done }">{{ todo.text }}</span>
+      <button @click="removeTodo(todo)">X</button>
+    </li>
+  </ul>
+  <button @click="hideCompleted = !hideCompleted">
+    {{ hideCompleted ? 'Mostrar' : 'Esconder' }}
+  </button>
+</template>
+
+<style>
+.done {
+  text-decoration: line-through;
+}
+</style>
